fce34df6b2fd1e083b7dfe8135e248a0bd0cbf25
Steve Calvert <scalvert@mobify.com>
2014-08-13 14:54:15 -0700

    Adding a number of other array implementations

--- app/arrays.js
+++ app/arrays.js
@@ -3,6 +3,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
+        console.log(arr);
+        console.log(item);
+
         for (var i = 0, l = arr.length; i < l; i++) {
             if (arr[i] === item) {
                 return i;
@@ -13,15 +16,37 @@ define(function() {
     },
 
     sum : function(arr) {
+        var total = 0;
 
+        for (var i = 0, l = arr.length; i < l; i++) {
+            total += arr[i];
+            console.log(total);
+        }
+
+        return total;
     },
 
     remove : function(arr, item) {
+        var temp = [];
 
+        for (var i = 0, l = arr.length; i < l; i++) {
+            if (arr[i] !== item) {
+                temp.push(arr[i]);
+            }
+        }
+
+        return temp;
     },
 
     removeWithoutCopy : function(arr, item) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i -= 1;
+            }
+        }
 
+        return arr;
     },
 
     append : function(arr, item) {
